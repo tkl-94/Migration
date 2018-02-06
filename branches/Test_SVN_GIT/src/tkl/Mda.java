c982bea9c0c691cfa48ae144f8e1ee449165fde2
ismail <ismail@6be8b360-5d3d-7a40-b41c-32cf9e40629d>
2018-02-06 11:32:05 +0000

    ok
    git-svn-id: http://localhost/svn/MIGRATION_DEPOT@14 6be8b360-5d3d-7a40-b41c-32cf9e40629d

---
+++ branches/Test_SVN_GIT/src/tkl/Mda.java
@@ -0,0 +1,55 @@
+package tkl;
+
+public class Mda {
+	private String name;
+	private String prenom;
+	private String ville;
+	private String pays;
+	
+	
+	
+	
+	public Mda(String name, String prenom, String ville, String pays) {
+		super();
+		this.name = name;
+		this.prenom = prenom;
+		this.ville = ville;
+		this.pays = pays;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	@Override
+	public String toString() {
+		return "Mda [name=" + name + ", prenom=" + prenom + ", ville=" + ville + ", pays=" + pays + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
