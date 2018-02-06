4c712dc6aee8f652479c1f9692acf0a02f3d270a
mahmoud <mahmoud@6be8b360-5d3d-7a40-b41c-32cf9e40629d>
2018-02-06 11:11:21 +0000

    comit 4
    git-svn-id: http://localhost/svn/MIGRATION_DEPOT@10 6be8b360-5d3d-7a40-b41c-32cf9e40629d

--- trunk/Test_SVN_GIT/src/tkl/Mda.java
+++ trunk/Test_SVN_GIT/src/tkl/Mda.java
@@ -6,6 +6,21 @@ public class Mda {
 	private String ville;
 	private String pays;
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
 	
 	
 
