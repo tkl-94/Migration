b18b816d1d71a9466e71e500d6d5c102aa225633
mahmoud <mahmoud@6be8b360-5d3d-7a40-b41c-32cf9e40629d>
2018-02-06 10:56:42 +0000

    First Comiit
    git-svn-id: http://localhost/svn/MIGRATION_DEPOT@3 6be8b360-5d3d-7a40-b41c-32cf9e40629d

---
+++ trunk/Test_SVN_GIT/src/tkl/Mda.java
@@ -0,0 +1,12 @@
+package tkl;
+
+public class Mda {
+	private String name;
+	private String prenom;
+	private String ville;
+	private String pays;
+	private String test;
+	private String val;
+	private String str;
+
+}
